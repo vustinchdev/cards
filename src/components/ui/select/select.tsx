03101aa435360598d92fff24a8da4e51ddb7bb57
vustinchdev <vustinchdev@gmail.com>
2024-03-14 17:01:51 +0300

    feat: add styles for select

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -2,30 +2,46 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import { ArrowIcon } from '@/assets/icons'
 import * as SelectPrimitive from '@radix-ui/react-select'
+import clsx from 'clsx'
+
+import s from './select.module.scss'
 
 import { Typography } from '../typography'
 
 type SelectProps = {
+  className?: string
   label?: string
   placeholder?: string
 } & ComponentPropsWithoutRef<typeof SelectPrimitive.Root>
 
 export const Select = forwardRef<ElementRef<typeof SelectPrimitive.Root>, SelectProps>(
-  ({ children, label, placeholder, ...rest }, ref) => {
+  ({ children, className, disabled, label, placeholder, ...rest }, ref) => {
+    const classNames = {
+      content: s.content,
+      icon: s.icon,
+      item: s.item,
+      label: clsx(label, disabled && s.disabledLabel),
+      trigger: s.trigger,
+    }
+
     return (
-      <div>
+      <div className={className}>
         {label && (
-          <Typography as={'label'} variant={'body2'}>
+          <Typography as={'label'} className={classNames.label} variant={'body2'}>
             {label}
           </Typography>
         )}
-        <SelectPrimitive.Root {...rest}>
-          <SelectPrimitive.Trigger ref={ref}>
+        <SelectPrimitive.Root disabled={disabled} {...rest}>
+          <SelectPrimitive.Trigger className={classNames.trigger} ref={ref}>
             <SelectPrimitive.Value placeholder={placeholder} />
-            <ArrowIcon />
+            <ArrowIcon className={classNames.icon} />
           </SelectPrimitive.Trigger>
           <SelectPrimitive.Portal>
-            <SelectPrimitive.Content>
+            <SelectPrimitive.Content
+              className={classNames.content}
+              position={'popper'}
+              side={'bottom'}
+            >
               <SelectPrimitive.Viewport>
                 <SelectPrimitive.Group>{children}</SelectPrimitive.Group>
               </SelectPrimitive.Viewport>
@@ -40,9 +56,9 @@ export const Select = forwardRef<ElementRef<typeof SelectPrimitive.Root>, Select
 type SelectItemProps = ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
 
 export const SelectItem = forwardRef<ElementRef<typeof SelectPrimitive.Item>, SelectItemProps>(
-  ({ children, ...rest }, ref) => {
+  ({ children, className, ...rest }, ref) => {
     return (
-      <SelectPrimitive.Item {...rest} ref={ref}>
+      <SelectPrimitive.Item {...rest} className={clsx(s.item, className)} ref={ref}>
         <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
       </SelectPrimitive.Item>
     )
