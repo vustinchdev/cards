9bed36077d515d8dbbe92f9d1bd4da442eae83b2
vustinchdev <vustinchdev@gmail.com>
2024-03-18 11:54:52 +0300

    feat: add icons for input

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
 export { ArrowIcon } from './arrow'
 export { BackArrowIcon } from './backArrow'
 export { CheckboxIndicatorIcon } from './checkbox'
+export { CloseIcon } from './close'
+export { EyeIcon } from './eye'
+export { SearchIcon } from './search'
