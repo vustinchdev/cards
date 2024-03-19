09d936d5f6ca22c959eb21e214931ed0b8eee717
vustinchdev <vustinchdev@gmail.com>
2024-03-19 13:08:58 +0300

    feat: add form-checkbox

---
+++ src/components/ui/form-components/form-checkbox/form-checkbox.tsx
@@ -0,0 +1,35 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { Checkbox, CheckboxProps } from '@/components/ui/checkbox'
+
+type Props<T extends FieldValues> = UseControllerProps<T> &
+  Omit<CheckboxProps, 'checked' | 'onBlur' | 'onCheckedChange'>
+
+export const FormCheckbox = <T extends FieldValues>({
+  control,
+  disabled,
+  name,
+  shouldUnregister,
+  ...rest
+}: Props<T>) => {
+  const {
+    field: { onBlur, onChange, ref, value },
+  } = useController({
+    control,
+    disabled,
+    name,
+    shouldUnregister,
+  })
+
+  return (
+    <Checkbox
+      checked={value}
+      disabled={disabled}
+      label={'remember me'}
+      onBlur={onBlur}
+      onCheckedChange={onChange}
+      ref={ref}
+      {...rest}
+    />
+  )
+}
