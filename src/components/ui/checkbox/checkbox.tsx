882b505011c835bdcd1dc665debe25f4fbe5aaec
vustinchdev <vustinchdev@gmail.com>
2024-03-19 11:28:03 +0300

    feat: add forwardRef for checkbox

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 
 import { CheckboxIndicatorIcon } from '@/assets/icons'
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
@@ -10,28 +10,30 @@ export type CheckboxProps = {
   label?: ReactNode
 } & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
 
-export const Checkbox = ({ className, disabled, id, label, ...rest }: CheckboxProps) => {
-  const classNames = {
-    buttonWrapper: clsx(s.buttonWrapper, disabled && s.disabled),
-    container: clsx(s.container, className),
-    indicator: clsx(s.indicator),
-    label: clsx(s.label, disabled && s.disabledLabel),
-  }
+export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>(
+  ({ className, disabled, id, label, ...rest }, ref) => {
+    const classNames = {
+      buttonWrapper: clsx(s.buttonWrapper, disabled && s.disabled),
+      container: clsx(s.container, className),
+      indicator: clsx(s.indicator),
+      label: clsx(s.label, disabled && s.disabledLabel),
+    }
 
-  return (
-    <div className={classNames.container}>
-      <div className={classNames.buttonWrapper}>
-        <CheckboxRadix.Root className={s.root} id={id} {...rest}>
-          <CheckboxRadix.Indicator className={classNames.indicator}>
-            <CheckboxIndicatorIcon />
-          </CheckboxRadix.Indicator>
-        </CheckboxRadix.Root>
+    return (
+      <div className={classNames.container}>
+        <div className={classNames.buttonWrapper}>
+          <CheckboxRadix.Root className={s.root} id={id} ref={ref} {...rest}>
+            <CheckboxRadix.Indicator className={classNames.indicator}>
+              <CheckboxIndicatorIcon />
+            </CheckboxRadix.Indicator>
+          </CheckboxRadix.Root>
+        </div>
+        {label && (
+          <label className={classNames.label} htmlFor={id}>
+            {label}
+          </label>
+        )}
       </div>
-      {label && (
-        <label className={classNames.label} htmlFor={id}>
-          {label}
-        </label>
-      )}
-    </div>
-  )
-}
+    )
+  }
+)
