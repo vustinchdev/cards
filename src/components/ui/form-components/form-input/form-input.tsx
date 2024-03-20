eb75e79bcc3a1234162396dfe43481642efc34e3
vustinchdev <vustinchdev@gmail.com>
2024-03-20 16:27:20 +0300

    feat: add form-input

---
+++ src/components/ui/form-components/form-input/form-input.tsx
@@ -0,0 +1,25 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { Input, InputProps } from '../../input'
+
+type Props<T extends FieldValues> = UseControllerProps<T> & Omit<InputProps, 'onChange' | 'value'>
+
+export const FormInput = <T extends FieldValues>({
+  control,
+  disabled,
+  name,
+  shouldUnregister,
+  ...rest
+}: Props<T>) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    control,
+    disabled,
+    name,
+    shouldUnregister,
+  })
+
+  return <Input {...field} {...rest} errorMessage={error?.message} />
+}
