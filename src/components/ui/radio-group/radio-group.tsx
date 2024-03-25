0da39fbf50b4d3477371e235c8c5c1bf02ee785b
vustinchdev <vustinchdev@gmail.com>
2024-03-25 13:57:34 +0300

    feat: add component radio-group

--- src/components/ui/radio-group/radio-group.tsx
+++ src/components/ui/radio-group/radio-group.tsx
@@ -1,3 +1,36 @@
-export const RadioGroup = () => {
-  return <div></div>
-}
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, useId } from 'react'
+
+import * as RagioGroupPrimitive from '@radix-ui/react-radio-group'
+
+export type RadioGroupProps = {
+  errorMessage?: string
+} & ComponentPropsWithoutRef<typeof RagioGroupPrimitive.Root>
+
+export const RadioGroup = forwardRef<ElementRef<typeof RagioGroupPrimitive.Root>, RadioGroupProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <RagioGroupPrimitive.Root ref={ref} {...rest}>
+        {children}
+      </RagioGroupPrimitive.Root>
+    )
+  }
+)
+
+export type RadioGroupItemProps = ComponentPropsWithoutRef<typeof RagioGroupPrimitive.Item>
+
+export const RadioGroupItem = forwardRef<
+  ElementRef<typeof RagioGroupPrimitive.Item>,
+  RadioGroupItemProps
+>(({ children, id: externalId, value, ...rest }, ref) => {
+  const id = useId()
+  const finalId = externalId ?? id
+
+  return (
+    <div>
+      <RagioGroupPrimitive.Item id={finalId} ref={ref} value={value} {...rest}>
+        <RagioGroupPrimitive.Indicator />
+      </RagioGroupPrimitive.Item>
+      <label htmlFor={finalId}>{children}</label>
+    </div>
+  )
+})
