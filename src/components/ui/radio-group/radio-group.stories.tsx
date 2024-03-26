4a072603b340c36f563b15802951b6688cab6c57
vustinchdev <vustinchdev@gmail.com>
2024-03-26 20:34:54 +0300

    feat: add story for radio-group

---
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { RadioGroup, RadioGroupItem } from './'
+
+const meta = {
+  argTypes: {},
+  component: RadioGroup,
+  tags: ['autodocs'],
+  title: 'Components/RadioGroup',
+} satisfies Meta<typeof RadioGroup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const RadioGroupDefault: Story = {
+  args: {
+    children: (
+      <>
+        <RadioGroupItem value={'option1'}>option 1</RadioGroupItem>
+        <RadioGroupItem value={'option2'}>option 2</RadioGroupItem>
+        <RadioGroupItem value={'option3'}>option 3</RadioGroupItem>
+      </>
+    ),
+  },
+}
+export const RadioGroupDisabled: Story = {
+  args: {
+    children: (
+      <>
+        <RadioGroupItem value={'option1'}>option 1</RadioGroupItem>
+        <RadioGroupItem value={'option2'}>option 2</RadioGroupItem>
+        <RadioGroupItem value={'option3'}>option 3</RadioGroupItem>
+      </>
+    ),
+    disabled: true,
+  },
+}
