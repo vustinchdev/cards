764718b3f3bafa758cad857d6e9d5911defb1fb3
vustinchdev <vustinchdev@gmail.com>
2024-04-02 12:25:35 +0300

    feat: add page and styles

--- src/components/ui/page/page.tsx
+++ src/components/ui/page/page.tsx
@@ -1,3 +1,21 @@
-export const Page = () => {
-  return <div></div>
-}
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+import clsx from 'clsx'
+
+import s from './page.module.scss'
+
+export type PageProps = ComponentPropsWithoutRef<'div'>
+
+export const Page = forwardRef<HTMLDivElement, PageProps>(
+  ({ children, className, ...rest }, ref) => {
+    const classNames = {
+      root: clsx(s.root, className),
+    }
+
+    return (
+      <div className={classNames.root} {...rest} ref={ref}>
+        {children}
+      </div>
+    )
+  }
+)
