c008c0fcd9c16a89e661a57e19662aabbfb63353
vustinchdev <vustinchdev@gmail.com>
2024-04-05 12:11:17 +0300

    feat: add grade and story

--- src/components/ui/grade/grade.tsx
+++ src/components/ui/grade/grade.tsx
@@ -1,3 +1,36 @@
-export const Grade = () => {
-  return <div></div>
+import { StarIcon } from '@/assets/icons/star'
+import { StarOutlineIcon } from '@/assets/icons/star-outline'
+
+type Props = {
+  maxGrade: number
+  onChangeGrade: (value: number) => void
+  value: number
+}
+
+export const Grade = ({ maxGrade, onChangeGrade, value }: Props) => {
+  const stars = Array.from({ length: maxGrade }, (_, i) => i + 1)
+
+  const handleClick = (value: number) => {
+    onChangeGrade(value)
+  }
+
+  return (
+    <div>
+      {stars.map(star => (
+        <Star key={star} onClick={handleClick} selected={value >= star} value={star} />
+      ))}
+    </div>
+  )
+}
+
+type StarProps = {
+  onClick: (value: number) => void
+  selected: boolean
+  value: number
+}
+
+const Star = ({ onClick, selected, value }: StarProps) => {
+  const handleClick = () => onClick(value)
+
+  return <span onClick={handleClick}>{selected ? <StarIcon /> : <StarOutlineIcon />}</span>
 }
