68775f19850cba25660390ad5822b262bbac53b2
vustinchdev <vustinchdev@gmail.com>
2024-04-17 15:34:45 +0300

    feat: add styles for modal

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -3,20 +3,30 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import { CloseIcon } from '@/assets'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 
+import s from './modal.module.scss'
+
 export type ModalProps = ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 
 export const ModalContent = forwardRef<ElementRef<typeof DialogPrimitive.Content>, ModalProps>(
-  ({ children, ...rest }, ref) => (
-    <DialogPrimitive.Portal>
-      <DialogPrimitive.Overlay />
-      <DialogPrimitive.Content {...rest} ref={ref}>
-        {children}
-        <DialogPrimitive.Close aria-label={'Close'}>
-          <CloseIcon />
-        </DialogPrimitive.Close>
-      </DialogPrimitive.Content>
-    </DialogPrimitive.Portal>
-  )
+  ({ children, ...rest }, ref) => {
+    const classNames = {
+      closeButton: s.closeButton,
+      content: s.content,
+      overlay: s.overlay,
+    }
+
+    return (
+      <DialogPrimitive.Portal>
+        <DialogPrimitive.Overlay className={classNames.overlay} />
+        <DialogPrimitive.Content {...rest} ref={ref}>
+          {children}
+          <DialogPrimitive.Close aria-label={'Close'} className={classNames.closeButton}>
+            <CloseIcon />
+          </DialogPrimitive.Close>
+        </DialogPrimitive.Content>
+      </DialogPrimitive.Portal>
+    )
+  }
 )
 
 export const Modal = DialogPrimitive.Root
