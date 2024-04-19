0db60adfdcedeca2a12b206a8bfba25546cd6c50
vustinchdev <vustinchdev@gmail.com>
2024-04-19 22:53:44 +0300

    feat: add forgot-password and story

---
+++ src/components/auth/forgot-password/forgot-password.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { MemoryRouter } from 'react-router-dom'
+
+import { ForgotPassword } from './'
+
+const meta = {
+  argTypes: {},
+  component: ForgotPassword,
+  tags: ['autodocs'],
+  title: 'Components/Auth/ForgotPassword',
+} satisfies Meta<typeof ForgotPassword>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ForgotPasswordDefault: Story = {
+  args: {
+    handlePasswordRecover: () => {},
+  },
+  render: args => {
+    return (
+      <MemoryRouter>
+        <ForgotPassword {...args} />
+      </MemoryRouter>
+    )
+  },
+}
--- src/components/auth/forgot-password/forgot-password.tsx
+++ src/components/auth/forgot-password/forgot-password.tsx
@@ -1,5 +1,49 @@
-import { Card } from '@/components/ui'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
-export const ForgotPassword = () => {
-  return <Card></Card>
+import { Button, Card, FormInput, Typography } from '@/components'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+type FormValues = z.infer<typeof emailSchema>
+const emailSchema = z.object({
+  email: z.string().email(),
+})
+
+type Props = {
+  handlePasswordRecover: (data: FormValues) => void
+}
+
+export const ForgotPassword = ({ handlePasswordRecover }: Props) => {
+  const { control, handleSubmit } = useForm<FormValues>({
+    resolver: zodResolver(emailSchema),
+  })
+
+  const onSubmit = (data: FormValues) => {
+    handlePasswordRecover(data)
+  }
+
+  return (
+    <Card as={'form'} onSubmit={handleSubmit(onSubmit)}>
+      <Typography as={'h1'} variant={'h1'}>
+        Forgot your password?
+      </Typography>
+      <FormInput
+        control={control}
+        label={'Email'}
+        name={'email'}
+        placeholder={'example@gmail.com'}
+      />
+      <Typography variant={'body2'}>
+        Enter your email address and we will send you further instructions
+      </Typography>
+      <Button fullWidth type={'submit'}>
+        Send Instructions
+      </Button>
+      <Typography variant={'body2'}>Did you remember your password?</Typography>
+      <Typography as={Link} to={'#'} variant={'subtitle1'}>
+        Try logging in
+      </Typography>
+    </Card>
+  )
 }
