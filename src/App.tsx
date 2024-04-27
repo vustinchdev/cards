e67dbfb51a24a7442c02d34c172e2488adf0d88c
vustinchdev <vustinchdev@gmail.com>
2024-04-27 17:40:00 +0300

    feat: add base-api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Provider } from 'react-redux'
 
 import { Router } from '@/router'
 
-import { store } from './services/store'
+import { store } from './services'
 
 export function App() {
   return (
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { baseApi } from './base-api'
+
 export const store = configureStore({
-  middleware: getDefaultMiddleware => getDefaultMiddleware(),
-  reducer: {},
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
 })
 
 export type AppDispatch = typeof store.dispatch
