b1f9f7406e0cb19b09b2a71a826f4b437f282c5b
vustinchdev <vustinchdev@gmail.com>
2024-04-29 21:12:03 +0300

    feat: add defaultAvatar to component avatar

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -1,15 +1,18 @@
 import { ComponentPropsWithoutRef } from 'react'
 
+import { defaultAvatar } from '@/assets'
 import clsx from 'clsx'
 
 import s from './avatar.module.scss'
 
-export type AvatarProps = ComponentPropsWithoutRef<'img'>
+export type AvatarProps = {
+  avatar?: string
+} & ComponentPropsWithoutRef<'img'>
 
-export const Avatar = ({ className, ...rest }: AvatarProps) => {
+export const Avatar = ({ avatar, className, ...rest }: AvatarProps) => {
   const classNames = {
     avatar: clsx(s.avatar, className),
   }
 
-  return <img className={classNames.avatar} {...rest} />
+  return <img className={classNames.avatar} src={avatar ?? defaultAvatar} {...rest} />
 }
