28214db043b4d34c5f6342b832facb3e99b33372
vustinchdev <vustinchdev@gmail.com>
2024-04-29 21:20:44 +0300

    feat: add story for header

---
+++ src/components/layout/header/header.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { MemoryRouter } from 'react-router-dom'
+
+import { defaultAvatar } from '@/assets'
+
+import { Header } from '.'
+
+const meta = {
+  argTypes: {},
+  component: Header,
+  tags: ['autodocs'],
+  title: 'Components/Layout/Header',
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const HeaderLoggedIn: Story = {
+  args: {
+    isLoggedIn: false,
+  },
+  render: args => {
+    return (
+      <MemoryRouter>
+        <Header {...args} />
+      </MemoryRouter>
+    )
+  },
+}
+export const HeaderLoggedOut: Story = {
+  args: {
+    avatar: defaultAvatar,
+    email: 'example@gmail.con',
+    isLoggedIn: true,
+    name: 'user',
+    onSignOut: () => {},
+  },
+  render: args => {
+    return (
+      <MemoryRouter>
+        <Header {...args} />
+      </MemoryRouter>
+    )
+  },
+}
