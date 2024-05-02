54f8bda9657838517a33f3c8572a38fbd04d9526
vustinchdev <vustinchdev@gmail.com>
2024-05-02 18:37:21 +0300

    feat: add app-slice

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,3 +1,4 @@
+import { appReducer } from '@/app-slice'
 import { configureStore } from '@reduxjs/toolkit'
 
 import { baseApi } from './base-api'
@@ -5,6 +6,7 @@ import { baseApi } from './base-api'
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
   reducer: {
+    app: appReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
 })
