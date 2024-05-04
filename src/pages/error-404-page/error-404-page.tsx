2724580aae42b6af4b76e3c6a702227efd7e9bb8
vustinchdev <vustinchdev@gmail.com>
2024-05-04 08:57:55 +0300

    feat: add styles for error-404-page

--- src/pages/error-404-page/error-404-page.tsx
+++ src/pages/error-404-page/error-404-page.tsx
@@ -3,14 +3,26 @@ import { Link } from 'react-router-dom'
 import { error404 } from '@/assets'
 import { Button, Page, Typography } from '@/components'
 
+import s from './error-404-page.module.scss'
+
 export const Error404Page = () => {
+  const classNames = {
+    buttonBack: s.buttonBack,
+    image: s.image,
+    text: s.text,
+  }
+
   return (
     <Page>
-      <img alt={'Page not found'} src={error404} />
-      <Typography variant={'body1'}>Sorry! Page not found!</Typography>
-      <Button as={Link} to={'/'}>
-        Back to home page
-      </Button>
+      <div>
+        <img alt={'Page not found'} className={classNames.image} src={error404} />
+        <Typography className={classNames.text} variant={'body1'}>
+          Sorry! Page not found!
+        </Typography>
+        <Button as={Link} className={classNames.buttonBack} to={'/'}>
+          Back to home page
+        </Button>
+      </div>
     </Page>
   )
 }
